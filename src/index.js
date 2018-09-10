19fa0be98bfcb2703135ef7448c3114854ada8ec
kaleniksasha <kalenik.wr@gmail.com>
2018-09-11 01:14:31 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-}
+  let match = equation.match(
+    /(-?\d+) \* x\^2 (-|\+) (-?\d+) \* x (-|\+) (-?\d+)/
+  );
+  let a = Number(match[1]);
+  let b = Number(match[2] + match[3]);
+  let c = Number(match[4] + match[5]);
+  let dsqrt = Math.sqrt(b * b - 4 * a * c);
+  let x1 = Math.round((-b + dsqrt) / (2 * a));
+  let x2 = Math.round((-b - dsqrt) / (2 * a));
+  return x1 > x2 ? [x2, x1] : [x1, x2];
+};
